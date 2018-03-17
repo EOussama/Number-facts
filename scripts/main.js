d7051f2b3b0e65e5e37da3e19de090ad4f4d624d
EOussama <oussama_gtacj@live.fr>
2018-03-17 18:11:19 +0000

    + Bug fixes. + Year facs added.

--- scripts/main.js
+++ scripts/main.js
@@ -3,45 +3,56 @@ window.addEventListener('load', function() { "use strict";
 		cardMath = document.getElementById('card-math'),
 		cardTrivia = document.getElementById('card-trivia'),
 		cardDate = document.getElementById('card-date'),
+		cardYear = document.getElementById('card-year'),
 		mathBtn = document.getElementById('mathBtn'),
 		triviaBtn = document.getElementById('triviaBtn'),
 		dateBtn = document.getElementById('dateBtn'),
+		yearBtn = document.getElementById('yearBtn'),
 		cbMath = document.getElementById('cbMath'),
 		cbTrivia = document.getElementById('cbTrivia'),
 		cbDate = document.getElementById('cbDate'),
+		cbYear = document.getElementById('cbYear'),
 		numberFact = document.getElementById('numberFact'),
 		triviaFact = document.getElementById('triviaFact'),
-		dateFact = document.getElementById('dateFact');
+		dateFact = document.getElementById('dateFact'),
+		yearFact = document.getElementById('yearFact');
 											
 											
 	function slide(slideId) {
 		switch(slideId) {
 			case 0: {
 				cardMath.style.display = 'block';
-				cardTrivia.style.display = cardDate.style.display = 'none';
+				cardTrivia.style.display = cardYear.style.display = cardDate.style.display = 'none';
 				
 				break;
 			}
 				
 			case 1: {
 				cardTrivia.style.display = 'block';
-				cardDate.style.display = cardMath.style.display = 'none';
+				cardDate.style.display = cardYear.style.display = cardMath.style.display = 'none';
 				
 				break;
 			}
 				
 			case 2: {
 				cardDate.style.display = 'block';
-				cardTrivia.style.display = cardMath.style.display = 'none';
+				cardTrivia.style.display = cardYear.style.display = cardMath.style.display = 'none';
+				
+				break;
+			}
+			case 3: {
+				cardYear.style.display = 'block';
+				cardTrivia.style.display = cardDate.style.display = cardMath.style.display = 'none';
 				
 				break;
 			}
 		}
 	}
 											
-	mathBtn.addEventListener('click', () => slide(0, mathBtn));
-	triviaBtn.addEventListener('click', () => slide(1, triviaBtn));
-	dateBtn.addEventListener('click', () => slide(2, dateBtn));
+	mathBtn.addEventListener('click', () => slide(0));
+	triviaBtn.addEventListener('click', () => slide(1));
+	dateBtn.addEventListener('click', () => slide(2));
+    yearBtn.addEventListener('click', () => slide(3));
 											
 	cbMath.addEventListener('input', function() {
 		if(cbMath.value != '') {
@@ -76,6 +87,16 @@ window.addEventListener('load', function() { "use strict";
 		
 		else {
 			dateFact.textContent = '';
+		}
+	});
+											
+	cbYear.addEventListener('input', function() {
+		if(cbYear.value != '') {
+		   	fetch(`http://numbersapi.com/${this.value}/year`).then((res) => res.text()).then((data) => yearFact.textContent = data);
+		}
+		
+		else {
+			yearFact.textContent = '';
 		}
 	});
 											
